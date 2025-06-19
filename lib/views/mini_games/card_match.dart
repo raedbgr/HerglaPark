ff234fe25ea60c4c95b669217c6bf6d0c14f915a
Raed Bouguerra <raed.bouguerra@gmail.com>
2025-06-19 18:52:36 +0100

    fixes

---
+++ lib/views/mini_games/card_match.dart
@@ -0,0 +1,15 @@
+import '/imports.dart';
+
+class CardMatch extends StatefulWidget {
+  const CardMatch({super.key});
+
+  @override
+  State<CardMatch> createState() => _CardMatchState();
+}
+
+class _CardMatchState extends State<CardMatch> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
